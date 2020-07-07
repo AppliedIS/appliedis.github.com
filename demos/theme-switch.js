c5e7f91d6d74f63b5bdcc4c74f103e931947604a
Matt Talda <matthew.talda@appliedis.com>
2020-07-07 16:59:30 -0400

    Updated logo, added icon for theme mode

--- demos/theme-switch.js
+++ demos/theme-switch.js
@@ -1,3 +1,5 @@
+/* Based of of https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8 */
+
 const toggleSwitch = document.querySelector('.mdl-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 
@@ -5,17 +7,24 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
+        document.getElementById("theme-icon").innerHTML= "brightness_2";
         toggleSwitch.checked = true;
+    }
+    else {
+        document.getElementById("theme-icon").innerHTML= "wb_sunny";
     }
 }
 
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        document.getElementById("theme-icon").innerHTML= "brightness_2";
         localStorage.setItem('theme', 'dark');
     }
-    else {        document.documentElement.setAttribute('data-theme', 'light');
-          localStorage.setItem('theme', 'light');
+    else {        
+        document.documentElement.setAttribute('data-theme', 'light');
+        document.getElementById("theme-icon").innerHTML= "wb_sunny";
+        localStorage.setItem('theme', 'light');
     }    
 }
 
