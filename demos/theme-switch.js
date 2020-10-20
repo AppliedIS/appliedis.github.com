a98dc54c15399e62d592b52b982a5bd024ea82ee
Matt Talda <matthew.talda@appliedis.com>
2020-10-20 15:54:20 -0400

    darkmode button, marketing, responsiveness

--- demos/theme-switch.js
+++ demos/theme-switch.js
@@ -1,34 +1,33 @@
 /* Based of https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8 */
 
-const toggleSwitch = document.querySelector('.mdl-switch input[type="checkbox"]');
+const toggleButton = document.querySelector('button');
 const currentTheme = localStorage.getItem('theme');
+let isDark = false;
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-  
     if (currentTheme === 'dark') {
-        document.getElementById("theme-icon").innerHTML= "brightness_2";
-        toggleSwitch.checked = true;
+        document.getElementById("theme-icon").innerHTML= "wb_sunny";
+        isDark = true;
     }
     else {
-        document.getElementById("theme-icon").innerHTML= "wb_sunny";
+        document.getElementById("theme-icon").innerHTML= "brightness_2";
     }
 }
 
 function switchTheme(e) {
-    if (e.target.checked) {
+    if (isDark == false) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        document.getElementById("theme-icon").innerHTML= "brightness_2";
+        document.getElementById("theme-icon").innerHTML= "wb_sunny";
         localStorage.setItem('theme', 'dark');
+        isDark = true;
     }
     else {        
         document.documentElement.setAttribute('data-theme', 'light');
-        document.getElementById("theme-icon").innerHTML= "wb_sunny";
+        document.getElementById("theme-icon").innerHTML= "brightness_2";
         localStorage.setItem('theme', 'light');
+        isDark = false;
     }    
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-
-
+toggleButton.addEventListener('click', switchTheme);
